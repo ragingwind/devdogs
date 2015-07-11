b20f361cc5df708caf17a35e62714eea20afe283
ragingwind <ragingwind@gmail.com>
2015-07-11 23:02:56 +0900

    Setup project

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+const app = require('app');
+const BrowserWindow = require('browser-window');
+
+// report crashes to the Electron project
+require('crash-reporter').start();
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+function createMainWindow () {
+	const win = new BrowserWindow({
+		width: 600,
+		height: 400,
+		resizable: false
+	});
+
+	win.loadUrl(`http://devdocs.io/`);
+	win.on('closed', onClosed);
+
+	return win;
+}
+
+function onClosed() {
+	// deref the window
+	// for multiple windows store them in an array
+	mainWindow = null;
+}
+
+// prevent window being GC'd
+let mainWindow;
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate-with-no-open-windows', function () {
+	if (!mainWindow) {
+		mainWindow = createMainWindow();
+	}
+});
+
+app.on('ready', function () {
+	mainWindow = createMainWindow();
+});
