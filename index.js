eda19a96326d37d29d6773b7aad1f9b374fb79a6
ragingwind <ragingwind@gmail.com>
2015-10-13 07:22:17 +0900

    Fix package error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 	require('electron-debug')();
 }
 
-require('electron-menu-loader')('menu', [process.platform]);
+require('electron-menu-loader')(path.join(__dirname, './menu'), [process.platform]);
 
 // prevent window being GC'd
 let win = null;
